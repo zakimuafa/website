5bc3a6762a00580ae0eca1a1cbaf777b11517118
Maretha Aulya Pasha W <marethaulya@gmail.com>
2024-10-08 14:38:12 +0700

    menampilkan gambar di tabel data barang admin

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -24,8 +24,8 @@
         <td><?php echo $brg->kategori ?></td>
         <td><?php echo $brg->harga ?></td>
         <td><?php echo $brg->stok ?></td>
-        <td><?php echo $brg->gambar ?></td>
-        <td><div class="btn btn-success btn-sm"><i class="fas fa-search-plus"></i></div></td>
+        <td><img src="<?php echo base_url(). '/uploads/'.$brg->gambar ?>" class="card-img-top" style="width: 200px; height: 200px; object-fit: cover;"></td>
+        <td><?php echo anchor('admin/data_barang/detail/' .$brg->id_brg, '<div class="btn btn-success btn-sm"><i class="fa fa-search"></i></div>') ?></td>
         <td><?php echo anchor('admin/data_barang/edit/' .$brg->id_brg, '<div class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></div>') ?></td>
         <td><?php echo anchor('admin/data_barang/hapus/' .$brg->id_brg, '<div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div>') ?></td>
     </tr>
@@ -92,3 +92,47 @@
     </div>
   </div>
 </div>
+
+<div class="card">
+        <h5 class="card-header">Detail Produk</h5>
+        <div class="card-body">
+
+            <?php foreach($barang as $brg) : ?>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php echo base_url(). '/uploads/'.$brg->gambar ?>" class="card-img-top">
+                </div>
+                <div class="col-md-8">
+                    <table class="table">
+                        <tr>
+                            <td>Nama Produk</td>
+                            <td><strong><?php echo $brg->nama_brg ?></strong></td>
+                        </tr>
+
+                        <tr>
+                            <td>Keterangan</td>
+                            <td><strong><?php echo $brg->keterangan ?></strong></td>
+                        </tr>
+
+                        <tr>
+                            <td>Kategori</td>
+                            <td><strong><?php echo $brg->kategori ?></strong></td>
+                        </tr>
+
+                        <tr>
+                            <td>Stok</td>
+                            <td><strong><?php echo $brg->stok ?></strong></td>
+                        </tr>
+
+                        <tr>
+                            <td>Harga</td>
+                            <td><strong><div class="btn btn-sm btn-success">Rp. <?php echo number_format($brg->harga, 0,',','.') ?></div></strong></td>
+                        </tr>
+                    </table>
+
+                    <?php echo anchor('admin/data_barang/index/', '<div class="btn btn-sm btn-danger">Kembali</div>') ?>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
