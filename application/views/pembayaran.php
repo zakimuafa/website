56b2a5b66fd5d66a225c8ed7694c21ef7f012060
zakimuafa <zakimuafa2@gmail.com>
2024-10-14 09:29:57 +0700

    pembayaran

--- application/views/pembayaran.php
+++ application/views/pembayaran.php
@@ -4,12 +4,13 @@
         <div class="col-md-8">
             <div class="btn btn-sm btn-success">
                 <?php 
-                $grand_total = 0;
-                if($keranjang = $this->cart->contents()) {
-                    foreach ($keranjang as $item) {
-                        $grand_total += $item['subtotal'];
-                    }
+                // Ambil nilai grand_total dari session
+                $grand_total = $this->session->userdata('grand_total');
+                if ($grand_total) {
                     echo "<h4>Total Belanja Anda: Rp. " . number_format($grand_total, 0, ',', '.') . "</h4>";
+                } else {
+                    echo "<h4>Total Belanja Anda: Rp. 0</h4>";
+                }
                 ?>
             </div><br><br>
 
@@ -17,25 +18,27 @@
 
             <form method="post" action="<?php echo base_url() ?>dashboard/proses_pesanan">
                 <input type="hidden" name="user_id" value="<?php echo $this->session->userdata('user_id'); ?>">
+                <input type="hidden" id="total_belanja" name="total_belanja" value="<?php echo $grand_total; ?>"> <!-- Input hidden untuk total belanja -->
 
                 <div class="form-group">
                     <label>Nama Lengkap</label>
-                    <input type="text" name="nama" id="nama" placeholder="Nama Lengkap Anda" class="form-control">
+                    <input type="text" name="nama" id="nama" placeholder="Nama Lengkap Anda" class="form-control" required>
                 </div>
 
                 <div class="form-group">
                     <label>Alamat Lengkap</label>
-                    <input type="text" name="alamat" id="alamat" placeholder="Alamat Lengkap Anda" class="form-control">
+                    <input type="text" name="alamat" id="alamat" placeholder="Alamat Lengkap Anda" class="form-control" required>
                 </div>
 
                 <div class="form-group">
                     <label>No. Telepon</label>
-                    <input type="text" name="no_telp" id="telepon" placeholder="Nomor Telepon Anda" class="form-control">
+                    <input type="text" name="no_telp" id="telepon" placeholder="Nomor Telepon Anda" class="form-control" required>
                 </div>
 
                 <div class="form-group">
                     <label>Metode Pengiriman</label>
-                    <select id="metode" name="metode" class="form-control">
+                    <select id="metode" name="metode" class="form-control" required>
+                        <option value="">-- Pilih Metode Pengiriman --</option>
                         <option>JNE</option>
                         <option>J&T</option>
                         <option>TIKI</option>
@@ -43,10 +46,11 @@
                         <option>POS Indonesia</option>
                     </select>
                 </div>
-                
+
                 <div class="form-group">
                     <label>Pilih Pembayaran</label>
-                    <select id="pembayaran" name="pembayaran" class="form-control">
+                    <select id="pembayaran" name="pembayaran" class="form-control" required>
+                        <option value="">-- Pilih Pembayaran --</option>
                         <option>BCA - XXXXXXX</option>
                         <option>BNI - XXXXXXX</option>
                         <option>BRI - XXXXXXX</option>
@@ -56,15 +60,11 @@
 
                 <button type="submit" id="pesan-button" class="btn btn-sm btn-primary mb-3" disabled>Pesan</button>
             </form>
-            <?php 
-                } else {
-                    echo "<h4>Keranjang Belanja Anda Masih Kosong!</h4>";
-                }
-            ?>
         </div>
         <div class="col-md-2"></div>
     </div>
 </div>
+
 
 <script>
     const namaInput = document.getElementById("nama");
@@ -91,6 +91,4 @@
     teleponInput.addEventListener("input", checkStatus);
     metodeSelect.addEventListener("change", checkStatus);
     pembayaranSelect.addEventListener("change", checkStatus);
-
-    
 </script>
